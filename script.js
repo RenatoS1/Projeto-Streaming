550c0c6c8a6c8e8fcf27b51dbfe7e9ea99aa6903
Renato Sena <158511024+RenatoS1@users.noreply.github.com>
2024-09-23 15:41:42 -0300

    adicionanco java script no nosso site

---
+++ script.js
@@ -0,0 +1,7 @@
+let lupa = document.getElementById('pesquisa');
+let imputi = document.getElementById('pexquisa');
+
+function inpt() {
+    imputi.style.top = '-53px'
+}
+lupa.addEventListener ('click', inpt)
