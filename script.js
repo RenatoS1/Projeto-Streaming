1b8d74d6a2808f143a77478119abeb21b7bb0967
Renato Sena <158511024+RenatoS1@users.noreply.github.com>
2024-09-25 16:01:59 -0300

    mexendo mais

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let lupa = document.getElementById('pesquisa');
 let imputi = document.getElementById('pexquisa');
 
 function inpt() {
-    imputi.style.top = '-53px'
+    imputi.style.top = '-53px';
 }
-lupa.addEventListener ('click', inpt)
+lupa.addEventListener ('click', inpt);
+function fechaInpt() {
+    imputi.style.top = '-150px';
+}
+lupa.addEventListener ('dblclick',fechaInpt);
