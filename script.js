102f44c140035c1ee5d572f10df37cf841876b39
Renato Sena <158511024+RenatoS1@users.noreply.github.com>
2024-10-05 14:56:07 -0300

    fazendo a interatividade

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ function fechaInpt() {
     imputi.style.top = '-150px';
 }
 lupa.addEventListener ('dblclick',fechaInpt);
+
+
+
+
+let menuCat = document.getElementById('menuCat');
+let voltarMenu = document.getElementById('voltar');
+let abreCategoria = document.getElementById('abreCategorias');
+function abreMenu() {
+    menuCat.style.left = '1px'; 
+}
+function fechaMenu() {
+    menuCat.style.left = '-410px'; 
+}
+
+voltarMenu.addEventListener ('click', fechaMenu);
+abreCategoria.addEventListener ('click', abreMenu);
